4a377b4dc11c918ee3d34e91f16a00abd6c67ac2
CampioniMan <campioni.daniel@gmail.com>
2025-06-22 23:41:45 -0300

    Added a lot of new data and models!

---
+++ AIBots/BigNormalizedNeuralNetworkBot.py
@@ -0,0 +1,30 @@
+from AIBots.BaseNeuralNetworkBot import BaseNeuralNetworkBot
+from AIBots.BaseNeuralNetworkBot import RangedClampNormalization
+import keras
+
+
+class BigNormalizedNeuralNetworkBot(BaseNeuralNetworkBot):
+	def __init__(self, file_path=None):
+		if file_path is None:
+			self.model = keras.models.Sequential([
+				keras.layers.Input(shape=(15,)),
+				RangedClampNormalization(44, range(0, 14)),
+				keras.layers.Dense(256, activation='relu'),
+				keras.layers.Dropout(0.1),
+				keras.layers.Dense(1024, activation='relu'),
+				keras.layers.Dropout(0.15),
+				keras.layers.Dense(512, activation='relu'),
+				keras.layers.Dropout(0.1),
+				keras.layers.Dense(256, activation='relu'),
+				keras.layers.Dropout(0.05),
+				keras.layers.Dense(128, activation='relu'),
+				keras.layers.Dense(6, activation='softmax')
+			])
+			self.model.compile(optimizer='adam',
+								loss='sparse_categorical_crossentropy',
+								metrics=['accuracy'])
+		else:
+			self.model = keras.models.load_model(file_path)
+
+	def get_title(self):
+		return f"BigNormalizedNeuralNetworkBot"
